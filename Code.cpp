1d80b8cbdcbff4ba5074ee014c75ce6afb4e8c60
Mohammed Sinad <61247376+mohdsinad@users.noreply.github.com>
2020-08-04 12:31:05 +0530

    Update Code.cpp

--- Code.cpp
+++ Code.cpp
@@ -7,101 +7,101 @@ sbit e = P3^2;
 sbit rd= P3^4;
 sbit wr= P3^5;
 sbit intr= P3^6;
-void delay(unsigned int);
-void lcd_cmd(unsigned char);
-void lcd_data(unsigned char);
-void lcd_data_string(unsigned char*);
-void lcd_init();
-void adc_conv();
-void adc_read();
-void lcd_data_adc(unsigned int);
-int num[10];
-float v_out,pressure;
+void delay (unsigned int);
+void lcd_cmd (unsigned char);
+void lcd_data (unsigned char);
+void lcd_data_string (unsigned char*);
+void lcd_init ();
+void adc_conv ();
+void adc_read ();
+void lcd_data_adc (unsigned int);
+int num [10];
+float v_out, pressure;
 
-void main()
+void main ()
 {
   dataport=0x00;
   adc_input=0xff;
   P3=0x00;
-  lcd_init();
+  lcd_init ();
   lcd_cmd(0x84);
   lcd_data_string("PRESSURE");
-  while(1)
+  while (1)
   {
     lcd_cmd(0xc5);
-    adc_conv();
-    adc_read();
-    lcd_data_string(" KPA");
-    delay(50);
+    adc_conv ();
+    adc_read ();
+    lcd_data_string (" KPA");
+    delay (50);
   }
 }
 
-void lcd_init()
+void lcd_init ()
 {
   lcd_cmd(0x01);
-  delay(1);
+  delay (1);
   lcd_cmd(0x06);
-  delay(1);
+  delay (1);
   lcd_cmd(0x0e);
-  delay(1);
+  delay (1);
   lcd_cmd(0x38);
-  delay(1);
+  delay (1);
   lcd_cmd(0x80);
-  delay(1);
+  delay (1);
 }
 
-void delay(unsigned int sec )
+void delay (unsigned int sec)
 {
-  int i ,j ;
-  for(i=0;i<sec;i++)
-    for(j=0; j<1275; j++);
+  int i, j;
+  for (i=0; i<sec; i++)
+    for (j=0; j<1275; j++);
 }
 
-void lcd_cmd(unsigned char item)
+void lcd_cmd (unsigned char item)
 {
   dataport=item;
   rs= 0;
   rw=0;
   e=1;
-  delay(1);
+  delay (1);
   e=0;
   return;
 }
 
-void lcd_data(unsigned char item)
+void lcd_data (unsigned char item)
 {
   dataport = item;
   rs= 1;
   rw=0;
   e=1;
-  delay(1);
+  delay (1);
   e=0;
 }
 
-void adc_conv()
+void adc_conv ()
 {
   wr = 0;
-  delay(2);
+  delay (2);
   wr = 1;
   while(intr);
-  delay(2);
+  delay (2);
   intr=1;
 }
 
-void adc_read()
+void adc_read ()
 {
   unsigned int value;
   rd = 0;
-  delay(2);
+  delay (2);
   value=adc_input;
-  v_out=(5.0/256)*value; //divide by 256 as it's a 8 bit converter
-  pressure=((v_out/5.0)+0.09)/0.009-1; // convert output voltage(volt) to KPA
-  delay(1);
+  v_out= (5.0/256) *value; //divide by 256 as it's an 8-bit converter
+  pressure=((v_out/5.0) +0.09)/0.009-1; // convert output voltage(volt) to KPA
+  delay (1);
   rd=1;
   lcd_data_adc(pressure);
 }
 
-void lcd_data_adc(unsigned int i)
+void lcd_data_adc (unsigned int i)
 {
   int p;
   int k=0;
@@ -112,23 +112,23 @@ void lcd_data_adc(unsigned int i)
     k++;
   }
   k--;
-  for (p=k;p>=0;p--)
+  for (p=k; p>=0; p--)
   {
     dataport=num[p]+48;
     rw = 0;
     rs = 1;
     e = 1;
-    delay(1);
+    delay (1);
     e = 0;
   }
 }
 
-void lcd_data_string(char *x)
+void lcd_data_string (char *x)
 {
-  while(*x!='\0')
+  while (*x! ='\0')
   {
     lcd_data(*x);
-    delay(2);
+    delay (2);
     x++;
   }
 }
